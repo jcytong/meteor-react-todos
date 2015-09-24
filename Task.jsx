ab1dc4b645d86c4383e98cbbe01c60caed1f2289
Johnny Tong <mailbox@johnnytong.com>
2015-09-24 01:52:48 -0400

    Ensure only owner can modify and see private tasks
    Add private prop to task
    Add button to toggle between Public/Private
    Remove auto publish package

--- Task.jsx
+++ Task.jsx
@@ -3,7 +3,8 @@ Task = React.createClass({
   propTypes: {
     // This component gets the task to display through a React prop.
     // We can use propTypes to indicate it is required
-    task: React.PropTypes.object.isRequired
+    task: React.PropTypes.object.isRequired,
+    showPrivateButton: React.PropTypes.bool.isRequired
   },
 
   toggleChecked() {
@@ -15,15 +16,25 @@ Task = React.createClass({
     Meteor.call("removeTask", this.props.task._id);
   },
 
+  togglePrivate() {
+    Meteor.call("setPrivate", this.props.task._id, ! this.props.task.private);
+  },
+
   render() {
     // Give tasks a different className when they are checked off,
-    //     // so that we can style them nicely in CSS
+    // so that we can style them nicely in CSS
+    // Add "checked" and/or "private" to the className when needed
     const taskClassName = this.props.task.checked ? "checked" : "";
 
     return (
       <li className={taskClassName}>
         <button className="delete" onClick={this.deleteThisTask}> &times;</button>
         <input type="checkbox" readOnly={true} checked={this.props.task.checked} onClick={this.toggleChecked} />
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate}>
+            { this.props.task.private ? "Private" : "Public" }
+          </button>
+        ) : ''}
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}</span>
       </li>
