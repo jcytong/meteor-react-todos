c07005595e1d1958e61a78b3a82128aa329cdc0c
Johnny Tong <mailbox@johnnytong.com>
2015-09-23 21:56:08 -0400

    Hook up React component
    Use ReactMeteorData mixin to pass data to react component from mongo
    Add css styles
    Update html to contain only react component
    Create Task component

---
+++ App.jsx
@@ -0,0 +1,32 @@
+// App component - represents the whole app
+App = React.createClass({
+  // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+
+  // Loads items from the Task collection and puts them on this.data.tasks
+  getMeteorData() {
+    return {
+      tasks: Tasks.find().fetch()
+    }
+  },
+
+  renderTasks() {
+    return this.data.tasks.map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
